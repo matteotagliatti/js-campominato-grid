5698d42ac8000fb58385eeb932a27991c6f97942
Matteo Tagliatti <matteotagliatti@gmail.com>
2022-03-04 17:10:56 +0100

    create cells based on button value

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-const grid = document.getElementById("grid");
+const buttons = document.querySelectorAll("button");
 
-/* Create Cells */
-for (let i = 0; i < 100; i++) {
-  const cell = document.createElement("div");
-  cell.innerHTML = `<span>${[i + 1]}</span>`;
-  grid.appendChild(cell);
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    createCells(buttons[i].value);
+  });
+}
+
+/**
+ * Create all the cells
+ * @param {*} value // stored in the html tag button, value attribute
+ */
+function createCells(value) {
+  const gridInner = document.querySelectorAll("#grid > div");
+
+  /* Check if the grid is empty. Then create grid cells */
+  if (gridInner.length === 0) {
+    const grid = document.getElementById("grid");
+
+    for (let i = 0; i < value; i++) {
+      const cell = document.createElement("div");
+      cell.innerHTML = `<span>${[i + 1]}</span>`;
+      grid.appendChild(cell);
+    }
+  }
 }
